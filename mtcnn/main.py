6fea495b70bca8c0075a19edf7b1216cd2840e28
Todd <myoung44@vols.utk.edu>
2018-03-02 15:43:06 -0500

    Saving :video_game:

--- mtcnn/main.py
+++ mtcnn/main.py
@@ -7,9 +7,10 @@ import numpy as np
 from parser import parse_args
 
 from data import Deidentified
+from model import MTCNN
 
 
-def train(epoch, train_loader):
+def train(epoch, train_loader, model, optimizer, args):
     """
     Train the model.
 
@@ -20,27 +21,61 @@ def train(epoch, train_loader):
 
     * `train_loader` [torch.utils.data.Dataloader]
         Data loader to load the test set.
+
+    * `model`: [Pytorch model class]
+        Instantiated model.
+
+    * `optimizer`: [torch.optim optimizer]
+        Optimizer for learning the model.
+
+    * `args`: [argparse object]
+        Parsed arguments.
     """
+    model.train()
+    for batch_idx, sample in enumerate(train_loader):
 
 
-def test(test_loader):
+
+def test(test_loader, model, args):
     """
     Test the model.
 
     * `test_loader`: [torch.utils.data.Dataloader]
         Data loader for the test set.
+
+    * `model`: [Pytorch model class]
+        Instantiated model.
+
+    * `args`: [argparse object]
+        Parsed arguments.
     """
 
 
 def main():
     args = parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-    train_data = Deidentified(data_path=args.data_dir+'/data/train', label_path=args.data_dir+'/labels/train')
-    test_data = Deidentified(data_path=args.data_dir+'/data/test', label_path=args.data_dir+'/labels/test')
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    train_data = Deidentified(
+        data_path=args.data_dir+'/data/train',
+        label_path=args.data_dir+'/labels/train'
+    )
+
+    test_data = Deidentified(
+        data_path=args.data_dir + '/data/test',
+        label_path=args.data_dir + '/labels/test'
+    )
 
     train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False)
 
+    model = MTCNN()
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
     for epoch in range(1, args.num_epochs + 1):
-            train(epoch, train_loader)
-            test(test_loader)
+            train(epoch, train_loader, model, optimizer, args)
+            test(test_loader, model, args)
--- mtcnn/parser.py
+++ mtcnn/parser.py
@@ -14,6 +14,16 @@ def parse_args():
     parser.add_argument('--data_dir', type=str, default='/Users/youngtodd/data/deidentified'
                         help='Root directory for the data')
     parser.add_argument('--num_epochs', type=int, default=50,
-                        help='Number of epochs to be run.')
+                        help='Number of epochs to be run [default 50]')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='learning rate [default: 0.01]')
+    parser.add_argument('--momentum', type=float, default=0.5,
+                        help='SGD momentum [default: 0.5]')
+    parser.add_argument('--no_cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--half_prec', default=False,
+                        help='Whether to train with half precision [default: False]')
+    parser.add_argument('--seed', type=int, default=1,
+                        help='random seed [default: 1]')
     args = parser.parse_args()
     return args
